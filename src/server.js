1b939ae5ffa38beffe17cca1a7b8245215bb7bd3
Quillzyy <shaquirearija1@gmail.com>
2024-11-07 04:31:38 +0000

    Initiate repository

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import Hapi from "@hapi/hapi";
+import routes from "./routes.js";
+
+async function init() {
+    const server = Hapi.server({
+        port: 5000,
+        host: "localhost",
+        routes: {
+            cors: {
+                origin: ["*"],
+            },
+        },
+    });
+
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server running on ${server.info.uri}`);
+}
+
+init();
